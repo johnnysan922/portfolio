428d035127c21bfb11f57da90c38cb1ede8beeef
Johnny Sanchez <johnnyss1999@gmail.com>
2023-02-24 14:53:05 -0500

    Changed Tailwind styling to CSS

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,15 @@
 import React from 'react'
+import './Home.css'
 import {HiArrowNarrowRight} from 'react-icons/hi'
 
 const Home = () => {
   return (
-    <div name='home' className='w-full h-screen bg-[#0a192f]'>
+    <div className='home'>
 
         {/* Container*/}
-        <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
-            <p className='text-gray-400'>Hi, my name is</p>
-            <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Johnny Sanchez</h1>
+        <div className='bio'>
+            <p>Hi, my name is</p>
+            <h1 className='name'>Johnny Sanchez</h1>
             <h2>I'm an aspiring Web Developer</h2>
             <p>I am currently working to improve my skills by 
                 building different types of projects.
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,10 @@
 import React, {useState} from 'react'
+import './Navbar.css'
 import {FaBars, FaTimes, FaGithub, FaLinkedin, FaFacebook} from 'react-icons/fa'
 import {HiOutlineMail} from 'react-icons/hi'
 import {BsFillPersonLinesFill} from 'react-icons/bs'
-import Logo from '../assets/Johnny.png'
+
+import ComputerIcon from '@mui/icons-material/Computer';
 
 const Navbar = () => {
     const[nav, setNav] = useState(false)
@@ -13,8 +15,7 @@ const Navbar = () => {
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
         <div>
-            <img src={Logo} alt = "Logo Image" style={{width: '70px'}}/>
-            
+            <ComputerIcon className='logo'/>
         </div>
 
         {/* Menu */}
